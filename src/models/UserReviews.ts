87375e5c9cc6cc43de066e45fc0ecc53f5224052
Ansh Patel <anshhmt2004@gmail.com>
2026-07-09 11:00:53 +0530

    Remooved Excessive FIles and kept the iner folder

--- src/models/UserReviews.ts
+++
@@ -1,1 +0,0 @@
-// Will Add in Future 
--- src/models/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as User } from "./User";
-export { default as Room } from "./Room";
-export { default as Booking } from "./Bookings";
-export { default as Payment } from "./Payments";
-
-export type { IUser } from "./User";
-export type { IRoom } from "./Room";
-export type { IBooking } from "./Bookings";
-export type { IPayment } from "./Payments";
